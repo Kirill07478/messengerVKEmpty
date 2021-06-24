ed4b57a46f74b68c2ca79c387867771d715745d8
Kirill <Kirill07478@gmail.com>
2021-06-24 14:05:00 +0300

    show my new empty app

---
+++ ClientVK/ClientVK/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClientVK
+//
+//  Created by Kiryusha Orlov on 24.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
